f627eb4d447c27f14303e5af075bd165b20c4922
Claudio <claudiodicaprio89@gmail.com>
2021-11-13 00:12:21 +0100

    text__aaaaaaa

--- js/script.js
+++ js/script.js
@@ -15,9 +15,18 @@ const title = [
     'Paradise'
 ];
 
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+];
+
 // generate the elements
 
 let items ='';
+let texts ='';
 
 for (let i = 0; i < imagesArray.length; i++){
     items += `
@@ -25,21 +34,33 @@ for (let i = 0; i < imagesArray.length; i++){
            <img src="${imagesArray[i]}" alt="${title[i]}">
     </div>
     `;
+
+    texts += `
+    <div class = "inner">
+      <h2>${title[i]}</h2>
+      <span>${text[i]}</span>
+    </div>  
+    `
 }
 
-console.log(items);
+// console.log(items);
 
 // let's put this elements inside the html
 
 const sx = document.querySelector('.sx');
 sx.innerHTML = items;
+const writ = document.querySelector('.written')
+writ.innerHTML = texts;
+console.log(writ)
 
 const anfitrione = document.getElementsByClassName('anfitrione');
 const object = document.getElementsByClassName('object');
-console.log(anfitrione[0]);
+const written = document.getElementsByClassName('inner')
+// console.log(anfitrione[0]);
 let deanfrit = 0 ;
 anfitrione[deanfrit].classList.add('deact');
 object[deanfrit].classList.add('active-object');
+written[deanfrit].classList.add('deact');
 
 // image goes
 
@@ -54,14 +75,17 @@ if(deanfrit < imagesArray.length - 1){
     // let\'s remove the class wich gaves visibility
     anfitrione[deanfrit].classList.remove('deact');
     object[deanfrit].classList.remove('active-object');
+    written[deanfrit].classList.remove('deact');
+
     // in order to first change the photo first and then add to the new hptoto the class, let's increment our declared variable wich is acting as a deact class's counter
 
     deanfrit++;
     //add deact to next element
     anfitrione[deanfrit].classList.add('deact');
     object[deanfrit].classList.add('active-object');
+    written[deanfrit].classList.add('deact');
 
-    console.log(deanfrit)
+    // console.log(deanfrit);
 }
 });
 
@@ -69,11 +93,13 @@ up.addEventListener("click" , function(){
     if(deanfrit > 0){
         anfitrione[deanfrit].classList.remove('deact');
         object[deanfrit].classList.remove('active-object');
+        written[deanfrit].classList.remove('deact');
 
         deanfrit--;
     
         anfitrione[deanfrit].classList.add('deact');
         object[deanfrit].classList.add('active-object');
+        written[deanfrit].classList.add('deact');
     }
     
 })
