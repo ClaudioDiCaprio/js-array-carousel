86bfec00d20b44bca3b1225c3e4faa59ba915caa
Claudio <claudiodicaprio89@gmail.com>
2021-11-12 19:15:22 +0100

    trough_images

--- js/script.js
+++ js/script.js
@@ -39,8 +39,26 @@ console.log(anfitrione[0]);
 let deanfrit = 0 ;
 anfitrione[deanfrit].classList.add('deact');
 
+// image goes
+
 const up = document.querySelector('.up');
 const down = document.querySelector('.down');
+
+// let's create an event , when it happens we'll make a change
+up.addEventListener("click", function(){
+
+if(deanfrit < imagesArray.length - 1){
+    
+    // let\'s remove the class wich gaves visibility
+    anfitrione[deanfrit].classList.remove('deact');
+
+    // in order to first change the photo first and then add to the new hptoto the class, let's increment our declared variable wich is acting as a deact class's counter
+
+    deanfrit++;
+    //add deact to next element
+    anfitrione[deanfrit].classList.add('deact');
+}
+});
 
 
 
